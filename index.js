4c154e0a011036617f2214f17a047fbd64502f9c
Al_by <edu.123gts@gmail.com>
2024-01-02 20:39:46 -0500

    v0.2 now with a log of the page

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ inquirer
     },
   ])
   .then((answers) => {
+    //code to create the qr code
     const url = answers.URL;
     var qr_png = qr.image(url);
     qr_png.pipe(fs.createWriteStream('qr.png'));
 
-    console.log("your qr is ready to use");
+    //this code is a create a log based in the user's data he put in the code
+    fs.writeFile('log.txt', `this qr has been created using : ${url}`, (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+    }); 
   })
   .catch((error) => {
     if (error.isTtyError) {
